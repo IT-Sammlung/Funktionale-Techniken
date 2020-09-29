c73e99d93334948d7080b8c7277aaeae09bf309a
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-09-29 18:46:16 +0200

    Update 04) Rekursion.js

--- 04) Rekursion.js
+++ 04) Rekursion.js
@@ -1,7 +1,7 @@
-      const fakultaet = function(x) {
+      const fakultaet = (x) => {
         if(x <= 1) {
           return x;
         }
         return x * fakultaet(x-1);
-      }
+      };
       console.log(fakultaet(5));
