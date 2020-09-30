b2e41a6897335d785dc1fb520784f23a538d1a21
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-09-30 18:29:40 +0200

    Update 05) Closures.js

--- 05) Closures.js
+++ 05) Closures.js
@@ -18,6 +18,21 @@
         }
       }
       const z1 = zaehler('z1');
-      z1();
-      z1();
-      z1();
+      z1(); // 0
+      z1(); // 1
+      z1(); // 2
+
+      //Selbe Closure anderst verwendet
+      const cache = () => {
+        let i=0;
+        const aufgabe = () => {
+          i++;
+          return i;
+        };
+        return aufgabe;
+      };
+
+      const speicher1 = cache();
+      console.log(speicher1()); // 0
+      console.log(speicher1()); // 1
+      console.log(speicher1()); // 2
