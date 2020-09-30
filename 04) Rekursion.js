19dd324b7366be66ee68cee77e4d30a6684883b8
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-09-30 17:03:38 +0200

    Update 04) Rekursion.js

--- 04) Rekursion.js
+++ 04) Rekursion.js
@@ -1,7 +1,18 @@
+      //Beispiel Fakultät berechnen
+      //!5 = 5*4*3*2*1
       const fakultaet = (x) => {
         if(x <= 1) {
           return x;
         }
         return x * fakultaet(x-1);
       };
-      console.log(fakultaet(5));
+      console.log(fakultaet(5)); // 120
+      
+      //Beispiel x-te Stelle von Fibonacci berechnen
+      //1,2,3,5,8,13,21.... <== 4te Stelle von Fibonacci = 5
+      let fibo = (x) => {
+        if(x<=1) {return 1;}
+        return fibbo(x-1) + fibbo(x-2);
+      };
+
+      console.log(fibbo(4)); // 5
