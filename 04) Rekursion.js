b9cc6419b154998cf8aa650a33a896df1b05f605
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-01 15:45:49 +0200

    Update 04) Rekursion.js

--- 04) Rekursion.js
+++ 04) Rekursion.js
@@ -1,29 +1,31 @@
-        //Einfaches Beispiel, ähnelt vom Prinzip einer While
-        const laufen = (x) => {
-        if(x<=0){
-          console.log("angekommen");
-          return 0;
-        }
-        console.log("1m laufen");
-        return laufen(x-1);
-      }
-      laufen(5);
+// Sehr einfaches Beispiel
+const laufen = (x) => {
+  if (x <= 0) {
+    console.log("angekommen");
+    return 0;
+  }
+  console.log("1m laufen");
+  return laufen(x - 1);
+}
+laufen(5);
 
-      //Beispiel Fakultät berechnen
-      //!5 = 5*4*3*2*1
-      const fakultaet = (x) => {
-        if(x <= 1) {
-          return x;
-        }
-        return x * fakultaet(x-1);
-      };
-      console.log(fakultaet(5)); // 120
-      
-    //Beispiel x-te Stelle von Fibonacci berechnen
-    //1,1,2,3,5,8,13,21.... <== 6te Stelle von Fibonacci = 8
-    let fibo = (x) => {
-      if(x<=2) {return 1;}
-      return fibo(x-1) + fibo(x-2);
-    };
+// Beispiel Fakultät berechnen
+// !5 = 5*4*3*2*1
+const fakultaet = (x) => {
+  if (x <= 1) {
+    return x;
+  }
+  return x * fakultaet(x - 1);
+};
+console.log(fakultaet(5)); // 120
 
-    console.log(fibo(6)); // 8
+// Beispiel x-te Stelle von Fibonacci berechnen
+// 1,1,2,3,5,8,13,21.... <== 6te Stelle von Fibonacci = 8
+let fibo = (x) => {
+  if (x <= 2) {
+    return 1;
+  }
+  return fibo(x - 1) + fibo(x - 2);
+};
+
+console.log(fibo(6)); // 8
